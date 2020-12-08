5db33bfc8a844e415ecb126eff72a97f368e6335
pikokr <parangee9706@gmail.com>
2020-12-08 13:25:49 +0900

    feat: logout

--- src/components/UserMenu.jsx
+++ src/components/UserMenu.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
-import {Person} from "@material-ui/icons";
-import {IconButton, ListItem, ListItemText, MenuItem, Popover} from "@material-ui/core";
+import {LockOpen, Person} from "@material-ui/icons";
+import {Avatar, IconButton, ListItem, ListItemIcon, ListItemText, MenuItem, Popover} from "@material-ui/core";
 
 const UserMenu = ({user}) => {
     const [anchorEl, setAnchorEl] = React.useState(null)
@@ -12,9 +12,26 @@ const UserMenu = ({user}) => {
             </IconButton>
             <Popover open={Boolean(anchorEl)} anchorEl={anchorEl} onClose={() => setAnchorEl(null)}>
                 <ListItem>
+                    <ListItemIcon>
+                        <Avatar src={user.avatarURL}/>
+                    </ListItemIcon>
                     <ListItemText primary={user.tag}/>
                 </ListItem>
-                <MenuItem>프로필</MenuItem>
+                <ListItem button>
+                    <ListItemIcon>
+                        <Person/>
+                    </ListItemIcon>
+                    <ListItemText primary="프로필"/>
+                </ListItem>
+                <ListItem button onClick={() => {
+                    localStorage.removeItem('token')
+                    window.location.reload()
+                }}>
+                    <ListItemIcon>
+                        <LockOpen/>
+                    </ListItemIcon>
+                    <ListItemText primary="로그아웃"/>
+                </ListItem>
             </Popover>
         </>
     );
--- src/queries.js
+++ src/queries.js
@@ -5,6 +5,7 @@ export const GET_CURRENT_USER = gql`
         me {
             id
             tag
+            avatarURL
         }
     }
 `
